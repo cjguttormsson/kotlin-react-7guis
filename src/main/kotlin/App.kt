c25af411d362865cdd1060fb7df6bf20b0fa174e
CJ Guttormsson <cj.guttormsson@gmail.com>
2022-05-28 13:50:35 -0600

    Use StrictMode when rendering the app

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,7 +1,10 @@
 import kotlinx.browser.document
+import react.StrictMode
 import react.create
 import react.dom.client.createRoot
 
 fun main() {
-    createRoot(document.getElementById("root") ?: error("No root!")).render(Launcher.create())
+    createRoot(document.getElementById("root") ?: error("No root!")).render(StrictMode.create {
+        Launcher {}
+    })
 }
