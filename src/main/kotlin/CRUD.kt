6fdca3a004063ee2ab3dd177ae1d39ecc4abb9fe
CJ Guttormsson <cj.guttormsson@gmail.com>
2022-06-04 17:57:43 -0600

    Redo the layout for CRUD with boostrap

--- src/main/kotlin/CRUD.kt
+++ src/main/kotlin/CRUD.kt
@@ -1,15 +1,14 @@
 import csstype.ClassName
 import org.w3c.dom.HTMLSelectElement
-import react.FC
-import react.Props
+import react.*
 import react.dom.html.InputType
 import react.dom.html.ReactHTML.button
+import react.dom.html.ReactHTML.div
 import react.dom.html.ReactHTML.input
 import react.dom.html.ReactHTML.option
-import react.dom.html.ReactHTML.p
 import react.dom.html.ReactHTML.select
-import react.useRef
-import react.useState
+import react.dom.html.ReactHTML.span
+import kotlin.js.json
 
 val CRUD = FC<Props> {
     var filterPrefix: String by useState("")
@@ -23,81 +22,120 @@ val CRUD = FC<Props> {
 
     val selectElem = useRef(null as HTMLSelectElement?)
 
-    p {
-        +"Filter Prefix: "
-        input {
-            type = InputType.text
-            value = filterPrefix
-            onChange = { e ->
-                filterPrefix = e.target.value
-            }
-        }
-    }
+    div {
+        className = "row justify-content-center".cn
 
-    p {
-        select {
-            ref = selectElem
-            size = 10
-            for ((name, surname) in names) {
-                option {
-                    className = ClassName(if (surname.startsWith(filterPrefix)) "unfiltered" else "filtered")
-                    +"$surname, $name"
+        div {
+            className = "col-4".cn
+            div {
+                className = "input-group mb-3".cn
+                span {
+                    className = "input-group-text".cn
+                    +"Filter Prefix: "
+                }
+                input {
+                    className = "form-control".cn
+                    type = InputType.text
+                    value = filterPrefix
+                    onChange = { e ->
+                        filterPrefix = e.target.value
+                    }
                 }
             }
-            onChange = { e ->
-                val idx = e.target.selectedIndex
-                currentName = names[idx].first
-                currentSurname = names[idx].second
+            div {
+                className = "mb-3".cn
+                select {
+                    ref = selectElem
+                    size = 10
+                    style = json("width" to "100%").unsafeCast<CSSProperties>()
+                    for ((name, surname) in names) {
+                        option {
+                            className = ClassName(if (surname.startsWith(filterPrefix)) "unfiltered" else "filtered")
+                            +"$surname, $name"
+                        }
+                    }
+                    onChange = { e ->
+                        val idx = e.target.selectedIndex
+                        currentName = names[idx].first
+                        currentSurname = names[idx].second
+                    }
+                }
+            }
+        }
+
+        div {
+            className = "col-4 my-auto".cn
+            div {
+                className = "input-group mb-3".cn
+                span {
+                    className = "input-group-text".cn
+                    +"Name"
+                }
+                input {
+                    className = "form-control".cn
+                    type = InputType.text
+                    value = currentName
+                    onChange = { e -> currentName = e.target.value }
+                }
+            }
+            div {
+                className = "input-group mb-3".cn
+                span {
+                    className = "input-group-text".cn
+                    +"Surname"
+                }
+                input {
+                    className = "form-control".cn
+                    type = InputType.text
+                    value = currentSurname
+                    onChange = { e -> currentSurname = e.target.value }
+                }
             }
         }
     }
 
-    p {
-        +"Name: "
-        input {
-            type = InputType.text
-            value = currentName
-            onChange = { e -> currentName = e.target.value }
-        }
-    }
-
-    p {
-        +"Surname: "
-        input {
-            type = InputType.text
-            value = currentSurname
-            onChange = { e -> currentSurname = e.target.value }
-        }
-    }
-
-    p {
-        button {
-            +"Create"
-            onClick = {
-                names = names.plusElement(currentName to currentSurname)
-                currentName = ""
-                currentSurname = ""
-                selectElem.current!!.selectedIndex = -1 // deselect
+    div {
+        className = "row justify-content-start mx-auto".cn
+        div {
+            className = "col-2 offset-md-2".cn
+            button {
+                className = "btn btn-primary".cn
+                style = json("width" to "100%").unsafeCast<CSSProperties>()
+                +"Create"
+                onClick = {
+                    names = names.plusElement(currentName to currentSurname)
+                    currentName = ""
+                    currentSurname = ""
+                    selectElem.current!!.selectedIndex = -1 // deselect
+                }
             }
         }
-        +" "
-        button {
-            +"Update"
-            onClick = {
-                val namesCopy = names.toMutableList()
-                selectElem.current?.selectedIndex?.also { namesCopy[it] = currentName to currentSurname }
-                names = namesCopy.toList()
+        div {
+            className = "col-2".cn
+            button {
+                className = "btn btn-outline-primary".cn
+                style = json("width" to "100%").unsafeCast<CSSProperties>()
+                +"Update"
+                onClick = {
+                    val namesCopy = names.toMutableList()
+                    selectElem.current?.selectedIndex?.also { namesCopy[it] = currentName to currentSurname }
+                    names = namesCopy.toList()
+                }
             }
         }
-        +" "
-        button {
-            +"Delete"
-            onClick = {
-                val namesCopy = names.toMutableList()
-                selectElem.current?.selectedIndex?.also { namesCopy.removeAt(it) }
-                names = namesCopy.toList()
-                currentName = ""
-                currentSurname = ""
+        div {
+            className = "col-2".cn
+            button {
+                className = "btn btn-outline-danger".cn
+                style = json("width" to "100%").unsafeCast<CSSProperties>()
+                +"Delete"
+                onClick = {
+                    val namesCopy = names.toMutableList()
+                    selectElem.current?.selectedIndex?.also { namesCopy.removeAt(it) }
+                    names = namesCopy.toList()
+                    currentName = ""
+                    currentSurname = ""
+                }
             }
         }
     }
