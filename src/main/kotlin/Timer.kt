6f0264367c830572f6aa5346c784272f9d050529
CJ Guttormsson <cj.guttormsson@gmail.com>
2022-06-04 17:27:06 -0600

    Redo the layout for Timer with bootstrap

--- src/main/kotlin/Timer.kt
+++ src/main/kotlin/Timer.kt
@@ -1,24 +1,22 @@
 import kotlinx.js.timers.Timeout
 import kotlinx.js.timers.clearInterval
 import kotlinx.js.timers.setInterval
-import react.FC
-import react.Props
+import react.*
+import react.dom.aria.AriaRole
 import react.dom.html.InputType
 import react.dom.html.ReactHTML.button
+import react.dom.html.ReactHTML.div
 import react.dom.html.ReactHTML.input
-import react.dom.html.ReactHTML.p
-import react.dom.html.ReactHTML.progress
-import react.useEffectOnce
-import react.useState
 import kotlin.js.Date
-import kotlin.math.roundToInt
-import kotlin.time.Duration
+import kotlin.js.json
 import kotlin.time.Duration.Companion.milliseconds
 
 val Timer = FC<Props> {
     var startTimeMs: Double by useState(Date.now())
     var currentTimeMs: Double by useState(startTimeMs)
     var durationMs: Double by useState(10_000.0)
+
+    val progress: Double = useMemo(currentTimeMs) { ((currentTimeMs - startTimeMs) / durationMs).coerceIn(0.0, 1.0) }
 
     useEffectOnce {
         val timeout: Timeout = setInterval(16.milliseconds) { currentTimeMs = Date.now() }
@@ -27,39 +25,73 @@ val Timer = FC<Props> {
         }
     }
 
-    p {
-        +"Elapsed Time: "
-        progress {
-            value = (currentTimeMs - startTimeMs).roundToInt().toString()
-            console.log("Before toDouble $durationMs")
-            console.log(Duration.INFINITE)
-            max = durationMs
-            console.log("After toDouble")
+
+    div {
+        className = "row align-items-baseline justify-content-center mb-3".cn
+        div {
+            className = "col-4 text-end fs-1".cn
+            +"Progress:"
         }
-    }
-    p {
-        +(((currentTimeMs - startTimeMs) / 1000).asDynamic().toFixed(1) as String)
-        +" s"
-    }
-    p {
-        +"Duration: "
-        input {
-            type = InputType.range
-            min = 1_000.0 // ms
-            max = 60_000.0 // ms
-            value = durationMs.toString()
-            onChange = { e ->
-                console.log(e.target.value)
-                e.target.value.toDoubleOrNull()?.also { durationMs = it }
+        div {
+            className = "col-4 lh-base".cn
+            div {
+                className = "progress".cn
+                div {
+                    className = "progress-bar".cn
+                    role = AriaRole.progressbar
+                    style = json(
+                        "width" to "${progress * 100.0}%", "transitionDuration" to "0.0s"
+                    ).unsafeCast<CSSProperties>()
+                }
             }
         }
     }
-    p {
-        button {
-            +"Reset"
-            onClick = {
-                startTimeMs = Date.now()
+    div {
+        className = "row justify-content-center mb-3".cn
+        div {
+            className = "col-4 text-end fs-1".cn
+            +"Total Elapsed Time:"
+        }
+        div {
+            className = "col-4 fs-1".cn
+            +(((currentTimeMs - startTimeMs) / 1000).asDynamic().toFixed(1) as String)
+            +" s"
+        }
+    }
+
+    div {
+        className = "row align-items-baseline justify-content-center mb-3".cn
+        div {
+            className = "col-4 text-end fs-1".cn
+            +"Duration:"
+        }
+        div {
+            className = "col-4 lh-base".cn
+            input {
+                className = "form-range".cn
+                type = InputType.range
+                min = 1_000.0 // ms
+                max = 60_000.0 // ms
+                value = durationMs.toString()
+                onChange = { e ->
+                    console.log(e.target.value)
+                    e.target.value.toDoubleOrNull()?.also { durationMs = it }
+                }
             }
+        }
+    }
+    div {
+        className = "row justify-content-center mb-3".cn
+        div {
+            className = "col-4 d-flex".cn
+            button {
+                className = "btn btn-outline-primary".cn
+                +"Reset Timer"
+                onClick = {
+                    startTimeMs = Date.now()
+                }
+            }
+
         }
     }
 }
